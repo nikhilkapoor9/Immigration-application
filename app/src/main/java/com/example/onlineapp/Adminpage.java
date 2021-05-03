acd36b31c3706cb696e5c9d8e77730339e45353d
Nikhil Kapoor <nkapoor1@isimtl.local>
2021-05-03 19:38:49 -0400

    student/teacher changes

--- app/src/main/java/com/example/onlineapp/Adminpage.java
+++ app/src/main/java/com/example/onlineapp/Adminpage.java
@@ -2,17 +2,40 @@ package com.example.onlineapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toolbar;
 
 public class Adminpage extends AppCompatActivity {
 
     public Toolbar toolbar;
+    Button addTeacher,addStudent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminpage);
 
+        addTeacher=(Button)findViewById(R.id.addTeacher);
+        addStudent=(Button)findViewById(R.id.addStudent);
+
+        addTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Adminpage.this,TeacherDetails.class);
+                startActivity(intent);
+            }
+        });
+
+        addStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Adminpage.this,StudentDetails.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
---
+++ app/src/main/java/com/example/onlineapp/TeacherDetails.java
@@ -0,0 +1,14 @@
+package com.example.onlineapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_details);
+    }
+}
